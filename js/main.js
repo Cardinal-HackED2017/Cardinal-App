8e983cebbc4151ab508a5a36bfdd5836acfb2e07
Steven Boddez <stevenboddez@gmail.com>
2017-01-28 15:01:01 -0700

    moved main map to new file

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 window.onload = function() {
-    var mainMapID = 'map'
+    views = [
+        new MainMap('map')
+    ];
 
-    var map = L.map(mainMapID, {center: [53.54, -113.49], zoom: 11, attributionControl: false});
-
-    // L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png').addTo(map);
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(map);
-    // L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png').addTo(map);
-
+    views.forEach(function(view) {
+        view.load();
+    });
 };
