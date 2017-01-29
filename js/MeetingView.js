858aa386d5df34fea0a2bb4a06ab07b887f71e54
Steven Boddez <stevenboddez@gmail.com>
2017-01-28 21:50:31 -0700

    Requests are more working; added some stuff to meetingview

--- js/MeetingView.js
+++ js/MeetingView.js
@@ -3,15 +3,42 @@ function MeetingView(sidebar, divID, meeting) {
     this.meeting = meeting;
 
     this.load = function() {
-        participants = d3.select(divID)
+        var participants = d3.select(divID)
             .append('div')
-            .classed('participants', true);
+            .classed('meetingSection', true);
 
-        participants.selectAll('div.participant')
+        participants.append('div')
+            .classed('sectionIdentifier', true)
+            .append('i')
+            .classed('fa fa-users aria-hidden', true);
+
+        participants.append('div')
+            .classed('sectionContents', true)
+            .selectAll('div.participant')
             .data(meeting.participants)
             .enter()
             .append('div')
             .classed('participant', true)
             .text(function(d) { return d; });
+
+
+        var times = d3.select(divID)
+            .append('div')
+            .classed('meetingSection', true);
+
+        times.append('div')
+            .classed('sectionIdentifier', true)
+            .append('i')
+            .classed('fa fa-clock-o aria-hidden', true);
+
+        times.append('div')
+            .classed('sectionContents', true)
+            .selectAll('div.time')
+            .data(meeting.times)
+            .enter()
+            .append('div')
+            .classed('meetingTime', true)
+            .text(function(d) { return d; });
+
     }
 }
--- js/MeetingsList.js
+++ js/MeetingsList.js
@@ -2,14 +2,17 @@ var meetings = [
 	{"name": "Meeting 1",
 	"description": "this is a cool meeting",
 	"participants": ["Steve", "Hailey"],
+	"times": [],
 	"id": "1"},
 	{"name": "Test Meeting",
 	"description": "this is another cool meeting",
 	"participants": ["Steve", "George", "Ryan"],
+	"times": [],
 	"id": "2"},
 	{"name": "Another Meeting",
 	"description": "this is a non-cool meeting",
 	"participants": ["Curtis"],
+	"times": [],
 	"id": "3"}
 ];
 
@@ -17,6 +20,8 @@ function MeetingsList(sidebar, meetingsListID) {
 	this.id = meetingsListID;
 
 	this.load = function() {
+		this.getMeetings();
+
 		d3.select(this.id)
 			.selectAll('.meetingButton')
 			.data(meetings)
@@ -28,5 +33,16 @@ function MeetingsList(sidebar, meetingsListID) {
 			}).on('click', function(d) {
 				sidebar.showMeeting(d);
 			});
+	}
+
+	this.getMeetings = function() {
+		d3.request("http://" + hostandport + "/meetings/")
+	        .mimeType("application/json")
+	        .header("Authorization", authToken)
+	        .response(function(xhr) { return JSON.parse(xhr.responseText); })
+	        .get(function(error, data) {
+				if (error) { console.log(error); }
+	    		else { console.log(data); }
+	        });
 	}
 }
